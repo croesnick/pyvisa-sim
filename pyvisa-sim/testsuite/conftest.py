51184803bc1cf2c2e7331dd33f9d00205f25a0e6
Carsten Rösnick-Neugebauer <croesnick@gmail.com>
2020-02-19 17:09:35 +0100

    Log level info for pytest; renamed py.typed

--- pyvisa-sim/testsuite/conftest.py
+++ pyvisa-sim/testsuite/conftest.py
@@ -4,6 +4,11 @@ import pytest
 import pyvisa
 
 
+def pytest_logger_config(logger_config):
+    logger_config.add_loggers(['pyvisa-sim'], stdout_level='debug')
+    # logger_config.set_log_option_default('')
+
+
 @pytest.fixture(scope='session')
 def resource_manager():
     rm = pyvisa.ResourceManager('@sim')
